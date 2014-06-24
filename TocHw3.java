88c835ead7920a702b49d60ede248fac0bb08f72
imadam33 <imadam0303@gmail.com>
2014-06-24 16:28:42 +0800

    v4

--- TocHw3.java
+++ TocHw3.java
@@ -1,3 +1,9 @@
+/* ******************
+ * 姓名：黃奕璁			*
+ * 學號：F74002191 	*
+ * 					*
+ *******************/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -5,7 +11,6 @@ import java.net.URL;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-import org.json.JSONObject;
 import org.json.JSONArray;
 
 public class TocHw3 {
@@ -14,7 +19,6 @@ public class TocHw3 {
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		con.setRequestMethod("GET");
-		//con.setRequestProperty("Accept-Language", url);
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
 		String inputline = "";
 		StringBuffer result = new StringBuffer();
@@ -25,34 +29,34 @@ public class TocHw3 {
 	}
 	
 	public static void main(String[] args){
-		TocHw3 jj = new TocHw3();
+		if(args.length != 4)
+			System.out.println("arg個數不對");
+		TocHw3 hw3 = new TocHw3();
 		String url = args[0];
 		String result;
 		int i;
 		try{
-			result = jj.sentHttpRequest(url);
-			//JSONObject jsonobject = new JSONObject(result);
-			//System.out.println(jsonobject);
+			result = hw3.sentHttpRequest(url);
 			JSONArray array = new JSONArray(result);
 			Pattern pattern1 = Pattern.compile(".*" + args[1] + ".*");
 			Pattern pattern2 = Pattern.compile(".*" + args[2] + ".*");
-			Pattern pattern3 = Pattern.compile(".*交易年月.*" + args[3] + ".*");
 			int total = 0, numberOfResult = 0;
+			int year = Integer.valueOf(args[3] + "00");
 			for(i = 0; i < array.length(); i++){
 				Matcher matcher1 = pattern1.matcher(array.get(i).toString());
 				Matcher matcher2 = pattern2.matcher(array.get(i).toString());
-				Matcher matcher3 = pattern3.matcher(array.get(i).toString());
-				if(matcher1.matches() && matcher2.matches() && matcher3.matches()){
+				int getyear = array.getJSONObject(i).getInt("交易年月");
+				if(matcher1.matches() && matcher2.matches() && getyear >= year){
 					numberOfResult++;
 					total += array.getJSONObject(i).getInt("總價元");
 				}
 			}
-			System.out.println(total / numberOfResult);
+			if(numberOfResult != 0)
+				System.out.println(total / numberOfResult);
+			else
+				System.out.println("沒有找到人和一筆資料符合要求");
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
-		
 	}
-	
-	
 }
